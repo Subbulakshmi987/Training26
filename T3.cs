c33071deb9e52d4bc0cf5d0cdeb79822d26b2955
Subbulakshmi <csubbulakshmi10@gmail.com>
2026-07-16 12:12:17 +0530

    T3 modified

--- T3.cs
+++ T3.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-public class Class1
-{
-	public Class1()
-	{
-	}
+namespace Training_26 {
+   internal class T3 {
+      static void Main () {
+         int[] arr = { 30, 31, 29, 32, 28, 27, 30, 25 };
+         for (int i = 0; i < arr.Length; i++) {
+            for (int j = i + 1; j < arr.Length; j++) {
+               if (arr[i] > arr[j]) {
+                  Console.WriteLine (j);
+                  break;
+               }
+            }
+         }
+      }
+   }
 }
