47101809ed53756faf32983d7834e8890140574e
Subbulakshmi <csubbulakshmi10@gmail.com>
2026-07-31 14:09:37 +0530

    T2: Magic square

--- Training26/Program.cs
+++ Training26/Program.cs
@@ -9,6 +9,57 @@ namespace Training26;
 
 class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      int[,] matrix =
+         {
+                 {2, 7, 6},
+                 {9, 5, 1},
+                 {4, 3, 8}
+
+        };
+      int magicSum = 0;
+
+      for (int j = 0; j < 3; j++) {
+         magicSum += matrix[0, j];
+      }
+      bool Ismagic = false;
+      for (int i = 0; i < matrix.GetLength (0); i++) {
+         int sum = 0;
+         for (int j = 0; j < matrix.GetLength (1); j++) {
+            sum += matrix[i, j];
+         }
+
+         if (magicSum == sum) {
+            Ismagic = true;
+         }
+      }
+
+      for (int j = 0; j < matrix.GetLength (1); j++) {
+         int columnsum = 0;
+         for (int i = 0; i < matrix.GetLength (1); i++) {
+            columnsum += matrix[j, i];
+         }
+
+         if (magicSum == columnsum) {
+            Ismagic = true;
+         }
+      }
+      int diagonal1 = 0;
+      for (int i = 0; i < 3; i++) {
+         diagonal1 += matrix[i, i];
+
+      }
+      if (magicSum == diagonal1) {
+         Ismagic = true;
+      }
+      int diagonal2 = 0;
+      for (int i = 0; i < 3; i++) {
+         diagonal2 += matrix[i, 2 - i];
+
+      }
+      if (magicSum == diagonal1) {
+         Ismagic = true;
+      }
+      if (Ismagic) Console.WriteLine ("True");
+      else Console.WriteLine ("False");
    }
 }
