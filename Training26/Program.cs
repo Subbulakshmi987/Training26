07edaabe65b572b9a51ba4572f4e1dc9c6e598b1
Subbulakshmi <csubbulakshmi10@gmail.com>
2026-07-31 14:57:28 +0530

    T3-Wait for a Cooler day

--- Training26/Program.cs
+++ Training26/Program.cs
@@ -9,6 +9,14 @@ namespace Training26;
 
 class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+      int[] arr = { 30, 31, 29, 32, 28, 27, 30, 25 };
+      for (int i = 0; i < arr.Length; i++) {
+         for (int j = i + 1; j < arr.Length; j++) {
+            if (arr[i] > arr[j]) {
+               Console.WriteLine (j);
+               break;
+            }
+         }
+      }
    }
 }
