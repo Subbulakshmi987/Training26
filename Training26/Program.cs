79de4c0d02e0f96226f8b004fad5323e88e33a18
Subbulakshmi <csubbulakshmi10@gmail.com>
2026-08-05 17:13:53 +0530

    Optimized solution

--- Training26/Program.cs
+++ Training26/Program.cs
@@ -6,33 +6,30 @@
 // Program to check whether a given matrix is a magic square.
 // ------------------------------------------------------------------------------------------------
 namespace Training26;
+
 class Program {
    static void Main () {
       int[,] matrix = {
-         {2,7,6 },
-         {9,5,1 },
-         {4,3,8 }
+         {2, 7, 6 },
+         {9, 5, 1 },
+         {4, 3, 8 }
       };
       Console.WriteLine (IsMagicSquare (matrix));
    }
    static bool IsMagicSquare (int[,] matrix) {
-      int diagonal1 = 0, magicSum = 0,rowSum=0,columnSum=0, diagonal2 = 0;
+      int magicSum = 0, diagonal1 = 0, diagonal2 = 0, rowSum, columnSum;
       for (int j = 0; j < 3; j++) magicSum += matrix[0, j];
       for (int i = 0; i < 3; i++) {
-         for (int j = 0; j < 3; j++) 
+         rowSum = 0;
+         columnSum = 0;
+         for (int j = 0; j < 3; j++) {
             rowSum += matrix[i, j];
-         if(magicSum != rowSum) return false;
+            columnSum += matrix[j, i];
+         }
+         if (magicSum != rowSum || magicSum != columnSum) return false;
+         diagonal1 += matrix[i, i];
+         diagonal2 += matrix[i, 3 - 1 - i];
       }
-      for (int j = 0; j < 3; j++) {
-         for (int i = 0; i < 3; i++)
-            columnSum += matrix[i, j];
-         if(magicSum != columnSum) return false;
-      }
-      for (int i = 0; i < 3; i++) diagonal1 += matrix[i, i];
-      if(magicSum != diagonal1) return false;
-      for (int i = 0; i < 3; i++)
-         diagonal2 += matrix[i, 3- 1 - i];
-      if(magicSum != diagonal2) return false;
-      return true;
+      return diagonal1 == magicSum && diagonal2 == magicSum;
    }
 }
