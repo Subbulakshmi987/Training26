6d5606a626a2c953c93933b20a6ecad00cbeca1a
Subbulakshmi <csubbulakshmi10@gmail.com>
2026-08-05 16:19:51 +0530

    Updated

--- Training26/Program.cs
+++ Training26/Program.cs
@@ -12,10 +12,12 @@ class Program {
       CoolerDay ();}
    static void CoolerDay () {
       int[] arr = { 30, 31, 29, 32, 28, 27, 30, 25 };
+      bool found = false;
       for (int i = 0; i < arr.Length; i++) {
          for (int j = i + 1; j < arr.Length; j++) {
             if (arr[i] > arr[j]) {
-               Console.WriteLine (j);
+               Console.WriteLine (j-i);
+               found = true;
                break;
             }
          }
