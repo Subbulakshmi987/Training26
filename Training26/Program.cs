03571d2009d67b7bbba13d351cc0f56c86c4ae3b
Subbulakshmi <csubbulakshmi10@gmail.com>
2026-08-31 11:47:12 +0530

    Added

--- Training26/Program.cs
+++ Training26/Program.cs
@@ -1,14 +1,59 @@
 ﻿// ------------------------------------------------------------------------------------------------
-// Training ~ A training program for new joinees at Metamation, Batch- July 2025.
+// Training ~ A training program for new joinees at Metamation, Batch- 2026.
 // Copyright (c) Metamation India.
-// ------------------------------------------------------------------
-// Program.cs
-// Program on branch main.
 // ------------------------------------------------------------------------------------------------
-namespace Training26;
+// Program.cs
+// Program to validate a word and calculate its score based on the given rules.
+// ------------------------------------------------------------------------------------------------
 
-class Program {
-   static void Main (string[] args) {
-      Console.WriteLine ("Hello, World!");
+namespace Training26 {
+   class Program {
+      static void Main () {
+         string allowedletters = "UXALTINE";
+         Console.Write ("Enter a word");
+         string word = Console.ReadLine ().ToUpper ();
+         if (word.Length < 4) {
+            Console.WriteLine ("Invalid word. The word must contain atleast 4 letters.");
+            return;
+         }
+         if (!word.Contains ('U')) {
+            Console.WriteLine ("Invalid word.The word must contain the letter 'U'.");
+            return;
+         }
+         foreach (char ch in allowedletters) {
+            if (!allowedletters.Contains (ch)) {
+               Console.WriteLine ("Invalid word.The word contains letters outside the allowed set.");
+               return;
+            }
+         }
+         int score;
+         if (word.Length == 4)
+            score = 1;
+         else
+            score = word.Length;
+         bool pangram = true;
+         foreach (char ch in allowedletters) {
+            if (!word.Contains (ch)) {
+               pangram = false;
+               break;
+            }
+         }
+         if (pangram) {
+            score += 7;
+            Console.WriteLine ("It is a Pangram");
+         } else {
+            Console.WriteLine ("Valid word");
+         }
+         Console.WriteLine ("Score: " + score);
+      }
    }
 }
+      
+
+         
+
+      
+
+       
+
+         
